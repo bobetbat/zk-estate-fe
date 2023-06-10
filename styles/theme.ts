437c69249af024674eccba0b40ff75596b590406
bobetbat <b.bobetayev.97@gmail.com>
2023-06-10 20:00:31 +0200

    feat: types, styles

---
+++ styles/theme.ts
@@ -0,0 +1,22 @@
+
+
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+export default theme;
