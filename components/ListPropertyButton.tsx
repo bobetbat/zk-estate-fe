c114e2fc01db583b98058906d4b057dcf185fac4
bobetbat <b.bobetayev.97@gmail.com>
2023-06-11 09:41:02 +0200

    fix: callback on lock collateral

--- components/ListPropertyButton.tsx
+++ components/ListPropertyButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Typography, Button, Stack } from '@mui/material';
 import { useContractFunction } from '../hooks/useContractFunction';
 import { listProperty } from '../config/contract';
@@ -16,17 +16,25 @@ export const ListPropertyButton: React.FC<{ config: any, title: string, callBack
     error,
   ] = useContractFunction(config);
 
+  const cb = useCallback(
+    async () => {
+      if (write) {
+        await write()
+      }
+      // console.log('callBack', callBack)
+      // console.log('success', success)
+      if (callBack) {
+        setTimeout(() => callBack(), 4000)
+
+      }
+    },
+    [loading, success, write, callBack],
+  )
+  // console.log(success)
   return (
-    <Stack maxWidth={'6rem'} >
+    <Stack maxWidth={'10rem'} >
       <Stack mt='2rem' gap={1} direction='row' justifyContent='space-evenly'>
-        <Button size='large' variant='contained' onClick={async () => {
-          if (write) {
-            write()
-          }
-          if (callBack) {
-            callBack()
-          }
-        }}>{title}</Button>
+        <Button size='large' variant='contained' onClick={cb}>{title}</Button>
       </Stack>
       <Typography variant="body1">{loading}</Typography>
       <Typography variant="body1">{error ?? ''}</Typography>
