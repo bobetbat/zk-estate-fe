7fc2be801e7e0133ff1ec2914868be96121e7a84
bobetbat <b.bobetayev.97@gmail.com>
2023-06-11 03:32:21 +0200

    feat: rent contract

--- config/contract.ts
+++ config/contract.ts
@@ -19,12 +19,12 @@ export const contracts: TContracts = {
   534353: {
     name: 'scrollAlfaTestnet',
     estate: '0xbefC6F3B404F2BF6B4c52E05c55BE15ee3Fe294d',
-    rent: '0x'
+    rent: '0x8b88a4f6CD209301031333BC20334613430dff03'
   },
   420: {
     name: 'OptimismTestnet',
     estate: '0xb00FCA39f7a2332508C5fDe9503C2021DEe524c7',
-    rent: '0x'
+    rent: '0x7e80FB6Bc2e60CD13aF28F44553ee586eACDAeEa'
   },
   5001: {
     name: 'mantleTestnet',
@@ -34,12 +34,12 @@ export const contracts: TContracts = {
   10200: {
     name: 'chiadoGnosisTestnet',
     estate: '0xFe8Fa89D8D861971F5E9061B3Cb90fcaE270338F',
-    rent: '0x'//
+    rent: '0x2D72FFD12d3B4E550486a788779C9C03C413CF0E'//
   },
   167005: {
     name: 'taikoTestnet',
     estate: '0xbefC6F3B404F2BF6B4c52E05c55BE15ee3Fe294d',
-    rent: '0x'//
+    rent: '0x8b88a4f6CD209301031333BC20334613430dff03'//
   },
   
 }
